f75c24465609795caffdc9b289549911a3ae1df1
freyesaviles <freyesaviles@uamv.edu.ni>
2026-08-31 19:36:20 -0600

    Primer Commit de la configuracion de IntelliJ IDEA

---
+++ src/main/java/com/uam/taller1_serviciosweb/Taller1ServiciosWebApplication.java
@@ -0,0 +1,13 @@
+package com.uam.taller1_serviciosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1ServiciosWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1ServiciosWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uam/taller1_serviciosweb/Taller1ServiciosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.taller1_serviciosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1ServiciosWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
