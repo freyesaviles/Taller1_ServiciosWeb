3cb55fdc954df1295a275090b157fee933f1f777
freyesaviles <freyesaviles@uamv.edu.ni>
2026-08-31 20:16:09 -0600

    feat: registrar productos desde JSON

--- src/main/java/com/uam/taller1_serviciosweb/controllers/ProductoController.java
+++ src/main/java/com/uam/taller1_serviciosweb/controllers/ProductoController.java
@@ -1,9 +1,12 @@
 package com.uam.taller1_serviciosweb.controllers;
 
 import com.uam.taller1_serviciosweb.models.Producto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,11 @@ public class ProductoController {
                 .findFirst()
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Producto> registrarProducto(@RequestBody Producto producto) {
+        productos.add(producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(producto);
     }
 }
