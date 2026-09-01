eca563c6e8a7c5929cd74b90633b8ae3aae2fcf3
Max Martinez <mamartinezo@uamv.edu.ni>
2026-08-31 19:55:37 -0600

    feat: crear modelo Producto con atributos y constructores

--- src/main/java/com/uam/taller1_serviciosweb/model/Producto.java
+++ src/main/java/com/uam/taller1_serviciosweb/model/Producto.java
@@ -1,4 +1,24 @@
 package com.uam.taller1_serviciosweb.model;
 
 public class Producto {
+
+    private int id;
+    private String nombre;
+    private String presentacion;
+    private String categoria;
+    private boolean disponible;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, String presentacion,
+                    String categoria, boolean disponible) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.presentacion = presentacion;
+        this.categoria = categoria;
+        this.disponible = disponible;
+    }
+
 }
