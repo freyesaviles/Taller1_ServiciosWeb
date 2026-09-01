0330bc34ab27eabccacdecb56d07b7bea92cd071
freyesaviles <freyesaviles@uamv.edu.ni>
2026-08-31 20:15:16 -0600

    feat: configurar controlador y catalogo en memoria

--- src/main/java/com/uam/taller1_serviciosweb/controllers/ProductoController.java
+++ src/main/java/com/uam/taller1_serviciosweb/controllers/ProductoController.java
@@ -1,4 +1,26 @@
 package com.uam.taller1_serviciosweb.controllers;
 
+import com.uam.taller1_serviciosweb.models.Producto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
 public class ProductoController {
+
+    private final List<Producto> productos = new ArrayList<>();
+
+    public ProductoController() {
+        productos.add(new Producto(1, "Café Tradicional", "50 g", "Café soluble", true));
+        productos.add(new Producto(2, "Café Tradicional", "100 g", "Café soluble", true));
+        productos.add(new Producto(3, "Café Tradicional", "200 g", "Café soluble", true));
+        productos.add(new Producto(4, "Café Descafeinado", "50 g", "Descafeinado", true));
+        productos.add(new Producto(5, "Café Descafeinado", "100 g", "Descafeinado", false));
+        productos.add(new Producto(6, "Café con Canela", "50 g", "Café saborizado", true));
+        productos.add(new Producto(7, "Café con Vainilla", "100 g", "Café saborizado", true));
+        productos.add(new Producto(8, "Café Premium", "200 g", "Café soluble", false));
+    }
 }
